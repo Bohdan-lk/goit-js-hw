3a6bd9b3e8174ed08575658703c5000c6f8df13c
Bogdan <bogdan.lysenko.k@gmail.com>
2019-12-01 16:01:24 +0200

    fix41

---
+++ module-1/js/task-04.js
@@ -0,0 +1,27 @@
+'use strict';
+let credits = 23580;
+let pricePerDroid = 3000;
+let message;
+
+let totalDroid = +prompt("Kоличество дроидов которые пользователь хочет купить");
+
+let totalPrice = totalDroid * pricePerDroid;
+
+
+if (totalPrice <= credits && totalPrice !== 0) {
+    {
+        message = `Вы купили ${totalDroid} дроида`
+    }
+    console.log(`Вы купили ${totalDroid} дроидов, на счету осталось ${credits - totalPrice} кредитов.`);
+} else if (totalPrice > credits) {
+    message = 'Недостаточно средств на счету!'
+}
+
+
+else {
+    message = "Отменено пользователем"
+}
+
+
+
+alert(message);
